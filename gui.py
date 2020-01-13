05fc7c7ad4152c9dad55da95c67c5bcbe5506cdf
unknown <kevinziadeh@gmail.com>
2020-01-13 20:09:58 +0200

    main design finished

--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+# from kivy.uix.widget import Widget
+# from kivy.uix.button import Button
+# from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.label import Label
+# from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 from kivy.lang import Builder
